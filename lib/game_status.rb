34d2f5a917de27bf9c7cab0a004d74c15baed3d3
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-02-29 22:51:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,7 +39,7 @@ def full?(board)
 def draw?(board)
 if full?(board) == true
   return true
-elsif full?(board) == false || won?(board) == true
+elsif full?(board) == false || won?(board) == win_combination
   return false
 end
 end
